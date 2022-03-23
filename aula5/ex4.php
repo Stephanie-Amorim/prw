d4ba1d167785890bbc37b7149e3b216832ad1601
stephanie <stephanie.amorim@aluno.ifsp.edu.br>
2022-03-23 11:44:07 -0300

    exercicio if e else

---
+++ aula5/ex4.php
@@ -0,0 +1,33 @@
+<?php
+   $num1 8;
+   $num2 5;
+   $num3 2;
+    
+   if ($num1 > $num2 && $num1 > $num3 ){
+       if ($num2> $num3 ){
+           echo "A ordem decrescente é:". $num1."". $num2."".$num3;
+       }
+       else {
+           echo $num1."".$num3."".$num2;
+       }
+   }
+
+   if ($num2 > $num1 && $num2 > $num3 ){
+    if ($num1> $num3 ){
+        echo "A ordem decrescente é:". $num2."". $num1."".$num3;
+    }
+    else {
+        echo $num2."".$num1."".$num3;
+    }
+}
+
+if ($num3 > $num1 && $num3 > $num2){
+    if ($num2> $num1 ){
+        echo "A ordem decrescente é:". $num3."". $num2."".$num1;
+    }
+    else {
+        echo $num3."".$num2."".$num1;
+    }
+}
+?>
+    
