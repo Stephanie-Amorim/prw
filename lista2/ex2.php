687d549fcda3f6e3ce8e7091b1b03f90dffb2a32
stephanie <stephanie.amorim@aluno.ifsp.edu.br>
2022-04-06 09:05:13 -0300

    exemplo2

--- lista2/ex2.php
+++ lista2/ex2.php
@@ -2,17 +2,17 @@
 $mes = '2';
 switch($mes){
     case '1' echo 'Janeiro';break; 
-    case '1' echo 'Fevereiro';break; 
-    case '1' echo 'Março';break;
-    case '1' echo 'Abril';break;
-    case '1' echo 'Maio';break;
-    case '1' echo 'Junho';break;
-    case '1' echo 'Julho';break;
-    case '1' echo 'Agosto';break;
-    case '1' echo 'Setembro';break;
-    case '1' echo 'Outubro';break;
-    case '1' echo 'Novembro';break;
-    case '1' echo 'Dezembro';break;
+    case '2' echo 'Fevereiro';break; 
+    case '3' echo 'Março';break;
+    case '4' echo 'Abril';break;
+    case '5' echo 'Maio';break;
+    case '6' echo 'Junho';break;
+    case '7' echo 'Julho';break;
+    case '8' echo 'Agosto';break;
+    case '9' echo 'Setembro';break;
+    case '10' echo 'Outubro';break;
+    case '11' echo 'Novembro';break;
+    case '12' echo 'Dezembro';break;
     default:echo'[ERRO] de digitação';
     break;
 }
